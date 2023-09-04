9eea1e42096929c3ad0885763e4effb9ff503670
2bell1 <108409815+2bell1@users.noreply.github.com>
2023-09-04 20:03:12 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.c
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.c
@@ -3,12 +3,12 @@
 #include <stdlib.h>
 
 int solution(int n) {
-     int answer = 0;
-
+     int answer=0;
+    int i;
     if (n == 1)
         return 1; // 1의 약수는 자기 자신만
 
-    for (int i = 1; i <= n / 2; i++) {
+    for (i = 1; i <= n / 2; i++) {
         if (n % i == 0) {
             answer += i;
         }
