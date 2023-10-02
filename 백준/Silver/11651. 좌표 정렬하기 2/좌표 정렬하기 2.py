d183162415c7bb289157fd55862d35bc8ca239bb
2bell1 <108409815+2bell1@users.noreply.github.com>
2023-10-02 15:26:46 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 4396 ms, Memory: 53604 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,9 @@
+num = int(input())
+x,y=[],[]
+for _ in range(num):
+    x_value, y_value = map(int, input().split())
+    x.append(x_value)
+    y.append(y_value)
+x, y = zip(*sorted(zip(x, y), key=lambda a: (a[1], a[0])))
+for a,b in zip(x,y):
+    print(f"{a} {b}")
