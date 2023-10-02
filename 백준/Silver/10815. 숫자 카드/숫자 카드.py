cb33007e590bf474140e3fe851ddd783d2c845c2
2bell1 <108409815+2bell1@users.noreply.github.com>
2023-10-02 17:49:15 +0900

    Update 숫자 카드.py

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,16 +1,12 @@
 import sys
 
 N = int(sys.stdin.readline())
-cards = list(map(int, sys.stdin.readline().split()))
+N_ls = set(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
-checks = list(map(int, sys.stdin.readline().split()))
+M_ls = list(map(int, sys.stdin.readline().split()))
 
-_dict = {}  # 속도는 dictionary!
-for i in range(len(cards)):
-    _dict[cards[i]] = 0  # 아무 숫자로 mapping
-
-for j in range(M):
-    if checks[j] not in _dict:
-        print(0, end=' ')
+for i in range(M):
+    if M_ls[i] in N_ls:
+        print("1", end=" ")
     else:
-        print(1, end=' ')
+        print("0", end=" ")
