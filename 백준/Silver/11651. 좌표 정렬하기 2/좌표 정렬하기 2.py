6c2e2b352b13d7827a64ea4059ac197ad3b3a601
2bell1 <108409815+2bell1@users.noreply.github.com>
2023-10-02 17:30:39 +0900

    Update 좌표 정렬하기 2.py

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,7 +1,8 @@
+import sys
 num = int(input())
 x,y=[],[]
 for _ in range(num):
-    x_value, y_value = map(int, input().split())
+    x_value, y_value = map(int,sys.stdin.readline().split())
     x.append(x_value)
     y.append(y_value)
 x, y = zip(*sorted(zip(x, y), key=lambda a: (a[1], a[0])))
