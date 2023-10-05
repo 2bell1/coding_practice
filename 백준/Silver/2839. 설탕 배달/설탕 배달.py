ec440bddaa991806c5e01a02673928bd09fec3df
2bell1 <108409815+2bell1@users.noreply.github.com>
2023-10-05 12:38:26 +0900

    Update 설탕 배달.py

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,20 +1,16 @@
 import sys
 
-n = int(sys.stdin.readline())
-if n % 5 == 0:  # 5으로 나눠떨어질 때
-    print(n // 5)
+A = int(sys.stdin.readline())
+ans = 0
+
+while A > 0:
+    if A % 5 == 0:
+        ans += A // 5
+        break
+    A -= 3
+    ans += 1
+
+if A < 0:
+    print(-1)
 else:
-    p = 0
-    while n > 0:
-        n -= 3
-        p += 1
-        if n % 5 == 0:  # 3kg과 5kg를 조합해서 담을 수 있을 때
-            p += n // 5
-            print(p)
-            break
-        elif n == 1 or n == 2:  # 설탕 봉지만으로 나눌 수 없을 때
-            print(-1)
-            break
-        elif n == 0:  # 3으로 나눠떨어질 때
-            print(p)
-            break
+    print(ans)
