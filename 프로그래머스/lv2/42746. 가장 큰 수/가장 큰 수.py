a8e9a0b7ad7e535fae4fc64b1435adddd17a8d58
2bell1 <108409815+2bell1@users.noreply.github.com>
2023-10-08 13:43:10 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,26 +1,19 @@
 from functools import cmp_to_key
 
-# 우선순위를 설정하는 비교 함수 정의
-def custom_sort(x, y):
-    x_str, y_str = str(x), str(y)
-    
-    # x를 앞에 붙였을 때와 뒤에 붙였을 때의 값 계산
-    x_with_prefix = int(x_str + y_str)
-    x_with_suffix = int(y_str + x_str)
-    
-    # 두 값 중 더 큰 값을 반환하여 우선순위 설정
-    if x_with_prefix > x_with_suffix:
+def comp(x,y):
+    xy = str(x)+str(y)
+    yx = str(y) + str(x)
+    if xy > yx:
         return 1
-    elif x_with_prefix < x_with_suffix:
+    elif yx>xy:
         return -1
     else:
         return 0
+    
 def solution(numbers):
     answer = ''
-    str_numbers = [str(x) for x in numbers]
-    str_numbers.sort(reverse=True,key=(cmp_to_key(custom_sort)))
-    
-    for i in str_numbers:
-        answer+=i
-    answer=int(answer)
+    numbers.sort(key=cmp_to_key(comp), reverse=True)
+    for i,c in enumerate(numbers):
+        answer += str(c)
+    answer = int(answer)
     return str(answer)
