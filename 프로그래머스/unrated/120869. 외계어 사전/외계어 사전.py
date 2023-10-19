c9303befad3a173ec127263676715b461ff4dfcf
2bell1 <108409815+2bell1@users.noreply.github.com>
2023-10-20 02:10:45 +0900

    [level unrated] Title: 외계어 사전, Time: 0.01 ms, Memory: 9.91 MB -BaekjoonHub

--- 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.py
@@ -1,19 +1,6 @@
-from collections import Counter
 def solution(spell, dic):
-    answer = 2
-    ln = len(spell)
-    counter={}
+    answer = 0
     for i, c in enumerate(dic):
-        if len(c) != ln:
-            continue
-        else:
-            counter= Counter(c)
-            for a, b in enumerate(spell):
-                if counter[b] != 1:
-                    answer = 2
-                    break
-                else:
-                    if a == (ln-1):
-                        answer = 1
-                        return answer
-    return answer
+        if not set(spell) - set(c):
+            return 1
+    return 2
