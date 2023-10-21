75bc99205b5bfa40f05c24b3bbb3718d4149680a
2bell1 <108409815+2bell1@users.noreply.github.com>
2023-10-21 23:22:24 +0900

    하노이의 탑.py

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -5,9 +5,10 @@ def solution(n):
         if n == 1:
             answer.append([src, tgt])
         else:
-            hanoi(src,inter,tgt,n-1) #젤 위에까지 들어가서 중간으로 이동
-            hanoi(src,tgt,inter,1)  #이동시키고 그 다음 위에꺼 3번로
-            hanoi(inter,tgt,src,n-1) #이동시킨후 2번쨰꺼 3번쨰로
+            # 시작, 타겟, 중간 재귀함에 따라 계속 바뀜유의
+            hanoi(src,inter,tgt,n-1) 
+            hanoi(src,tgt,inter,1)  
+            hanoi(inter,tgt,src,n-1) 
             
     hanoi(1,3,2,n)
     
